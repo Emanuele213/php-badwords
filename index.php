b304c7d6cf3459c7cb940114ff2ed8696072204a
Emanuele213 <vittorius213@gmail.com>
2022-12-13 16:18:52 +0100

    exercise completed

--- index.php
+++ index.php
@@ -10,14 +10,15 @@
 <body>
     <?php
         $text = "In un futuro prossimo venturo, il pianeta Terra sta diventando un ambiente inabitabile per l'uomo. Un gruppo di scienziati appartenenti un tempo alla NASA, sfruttando un wormhole per superare le limitazioni fisiche del viaggio spaziale e coprire le immense distanze del viaggio interstellare, organizza una serie di missioni spaziali alla ricerca di un pianeta abitabile. A tal scopo convince Cooper, un ex pilota di talento, a lasciare i propri figli per imbarcarsi in una missione che ha lo scopo di salvare l'umanità.";
+        $badWord = str_ireplace($_GET['badWord'], '***', $text);
     ?>
     <div class="container">
-        <h1>Interstellar</h1>
-        <p><?= $text; ?></p>
+        <h1>INTERSTELLAR</h1>
+        <p><?= $badWord; ?></p>
         <p class="num">Lunghezza stringa: <?php echo strlen($text); ?></p>
         <form action="" method="GET">
-            <input type="text" name="" placeholder="inserisci la parola da sbarrare">
-            <button class="mod-btn">Cancella</button>
+            <input type="text" name="badWord" placeholder="inserisci la parola da sbarrare">
+            <button class="mod-btn">Sbarra</button>
         </form>
     </div>
 </body>
