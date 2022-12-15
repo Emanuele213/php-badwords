8a7a75fb377adc87175fdeab97e9e6dad2659428
Emanuele213 <vittorius213@gmail.com>
2022-12-15 10:25:07 +0100

    add isset for error

--- index.php
+++ index.php
@@ -10,16 +10,17 @@
 <body>
     <?php
         $text = "In un futuro prossimo venturo, il pianeta Terra sta diventando un ambiente inabitabile per l'uomo. Un gruppo di scienziati appartenenti un tempo alla NASA, sfruttando un wormhole per superare le limitazioni fisiche del viaggio spaziale e coprire le immense distanze del viaggio interstellare, organizza una serie di missioni spaziali alla ricerca di un pianeta abitabile. A tal scopo convince Cooper, un ex pilota di talento, a lasciare i propri figli per imbarcarsi in una missione che ha lo scopo di salvare l'umanità.";
-        $badWord = str_ireplace($_GET['badWord'], '***', $text);
+        $badWord = str_ireplace(isset($_GET['badWord']), '***', $text, $numCensor);
     ?>
     <div class="container">
         <h1>INTERSTELLAR</h1>
         <p><?= $badWord; ?></p>
-        <p class="num">Lunghezza stringa: <?php echo strlen($text); ?></p>
+        <p class="num">Lunghezza stringa: <?php echo strlen($badWord); ?></p>
         <form action="" method="GET">
             <input type="text" name="badWord" placeholder="inserisci la parola da sbarrare">
             <button class="mod-btn">Sbarra</button>
         </form>
+        <h3 class="num-censor">Parole censurate: <?= $numCensor ?></h3>
     </div>
 </body>
 </html>
